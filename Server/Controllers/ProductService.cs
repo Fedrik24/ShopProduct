d8629b4baca7f2e9f6ea6fa1febd5cac28b48128
Fedrik24 <fedrikarya24@gmail.com>
2024-02-03 15:55:46 +0700

    - Chore : Edit Login Pages

---
+++ Server/Controllers/ProductService.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using ShopProduct.Server.Handler.Service;
+
+
+namespace ShopProduct.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductService : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ProductService(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet()]
+        public async Task<IActionResult> GetProductData()
+        {
+            var query = new GetProductDataAsyncQuery();
+            var result = await _mediator.Send(query);
+            return result;
+        }
+
+    }
+}
---
+++ Server/Handler/ServiceQuery/GetProductDataAsync.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ShopProduct.Server.Handler.Service
+{
+    public record GetProductDataAsyncQuery : IRequest<IActionResult>;
+
+    public class GetProductDataAsync : IRequestHandler<GetProductDataAsyncQuery, IActionResult>
+    {
+        public async Task<IActionResult> Handle(GetProductDataAsyncQuery request, CancellationToken cancellationToken)
+        {
+            return new OkObjectResult($"ITEM");
+        }
+    }
+}
