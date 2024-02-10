1d89f48acc7c23518bcf8bf84767d85c35a4b67d
Fedrik24 <fedrikarya24@gmail.com>
2024-02-10 15:34:51 +0700

    - Feat : Register UI Client

---
+++ Shared/RegisterData.cs
@@ -0,0 +1,10 @@
+﻿namespace ShopProduct.Shared
+{
+    public class RegisterData
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public DateTime BirthDay { get; set; }
+        public Gender Gender { get; set; }
+    }
+}
